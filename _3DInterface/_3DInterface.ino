c2414ffe8f0e0094352c08a55c94c18b5e261554
tjsherod <tjsherod@gmail.com>
2012-09-28 10:52:56 -0500

    Shorted comments

--- _3DInterface/_3DInterface.ino
+++ _3DInterface/_3DInterface.ino
@@ -10,9 +10,9 @@
 void setup() {
   Serial.begin(115200);
 
-  // unused pins are fairly insignificant,
-  // but pulled low to reduce unknown variables
-  for(int i = 2; i < 14; i++) {
+  // unused pins are fairly insignificant, but pulled low to reduce unknown variables
+  
+for(int i = 2; i < 14; i++) {
     pinMode(i, OUTPUT);
     digitalWrite(i, LOW);
   }
@@ -35,8 +35,7 @@ void loop() {
 long time(int pin, byte mask) {
   unsigned long count = 0, total = 0;
   while(checkTimer() < refresh) {
-    // pinMode is about 6 times slower than assigning
-    // DDRB directly, but that pause is important
+    // pinMode is about 6 times slower than assigning DDRB directly, but that pause is important
     pinMode(pin, OUTPUT);
     PORTB = 0;
     pinMode(pin, INPUT);
